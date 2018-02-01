631fc8c3706aae61d03266b0f3d6ce4d8552abbc
Ambar Home <a.srivastava@murmotorsports.com>
2018-02-02 09:40:37 +1100

    Efficiency bugs

--- Efficiency_Sim_iterate.m
+++ Efficiency_Sim_iterate.m
@@ -1,5 +1,6 @@
 function effScore = Efficiency_Sim_iterate(CO2conversionFactor, competition, enduroEnergy , enduroTime)
 Tmin = competition.enduranceMin;
+T_CO2min = competition.efficiency.T_CO2min;
 Tyours = enduroTime; 
 Lapyours = competition.laps;
 Laptotalmin = competition.laps;
@@ -12,7 +13,7 @@ TyoursEffMin = 1.45*competition.enduranceMin;
 
 effFactorYours = ((Tmin/Laptotalmin)/(Tyours/Lapyours))*((CO2min/LaptotalCO2min)/(CO2yours/Lapyours));
 effFactorMin = ((Tmin/Laptotalmin)/(TyoursEffMin/Lapyours))*((CO2min/LaptotalCO2min)/(CO2yoursEffMin/Lapyours));
-effFactorMax = ((Tmin/Laptotalmin)/(Tmin/Laptotalmin))*((CO2min/LaptotalCO2min)/(CO2min/LaptotalCO2min));
+effFactorMax = ((Tmin/Laptotalmin)/(T_CO2min/Laptotalmin))*((CO2min/LaptotalCO2min)/(CO2min/LaptotalCO2min));
 
 effScore = 100*((effFactorMin/effFactorYours)-1)/((effFactorMin/effFactorMax)-1);
 end
--- competition_properties_2017_E_FSAE_AUS.m
+++ competition_properties_2017_E_FSAE_AUS.m
@@ -5,13 +5,14 @@ competition.trackData = loadTrackData('2016TrackDataVer2');
 competition.trackDistance = sum(competition.trackData(:,1));
 competition.laps = 20;
 competition.totalDistance = competition.trackDistance*competition.laps;
+competition.totalDistance = 24000; %Temporary
 
 %% Scores
 competition.autocrossMin = 75;
-competition.enduranceMin = 1654.5; 
+competition.enduranceMin = 1654.5;
 competition.accelMin = 4.231;
 competition.skidMin = 5.306;
 
 %% Efficiency
-competition.efficiency.LapTotalCO2min = 1723.5;
-competition.efficiency.CO2min = 4*0.65; % UTS Electric
+competition.efficiency.T_CO2min = 1723.5; % UTS Electric
+competition.efficiency.CO2min = 4*0.65; % 4kWh - UTS Electric
